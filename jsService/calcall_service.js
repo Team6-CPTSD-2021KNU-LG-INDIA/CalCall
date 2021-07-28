a0399bc51095d7366f17cc592caf7cdb39f70893
LeeHanSeong <jkhk7788@gmail.com>
2021-07-29 04:36:17 +0900

    update

--- jsService/calcall_service.js
+++ jsService/calcall_service.js
@@ -1,12 +1,28 @@
 import applyDB from './sub-services/dbService'
 import applyEventservice from './sub-services/eventService'
 
-
 const pkgInfo = require('./package.json');
 const Service = require('webos-service');
 const service = new Service(pkgInfo.name);
 
 (()=>{
     //applyDB(pkgInfo,service, pkgInfo.name+':1');
-    applyEventservice(pkgInfo,service,'/home/root/CalCall/eventDatas');
-})
+    //applyEventservice(pkgInfo,service,'/home/root/CalCall/eventDatas');
+    service.register("getEventlist", function(message) {
+        message.respond({
+            returnValue: true,
+            Response: {
+                '0':{
+                    title: 'All Day Event',
+                    start: '2021-07-01'
+                  },
+                 '1': {
+                    title: 'Long Event',
+                    start: '2021-07-01',
+                    end: '2021-07-10',
+                    color: 'purple' // override!
+                  },
+            }
+        });
+    });
+})();
