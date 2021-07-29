f192f1ae8510f12b4e78139e44c5049fa4455ec8
LeeHanSeong <jkhk7788@gmail.com>
2021-07-30 02:08:40 +0900

    update

--- jsService/calcall_service.js
+++ jsService/calcall_service.js
@@ -1,17 +1,12 @@
 import {applyDB} from './sub-services/dbService'
 import {applyEventservice} from './sub-services/eventService'
+const config = require('./config.json');
 
 const pkgInfo = require('./package.json');
 const Service = require('webos-service');
 const service = new Service(pkgInfo.name);
 
 (()=>{
-    //applyDB(pkgInfo,service, pkgInfo.name+':1');
-    //applyEventservice(pkgInfo,service,'/home/root/CalCall/eventDatas');
-    service.register("getEventlist", function(message) {
-        message.respond({
-            returnValue: true,
-            Response: 'whywhywhy',
-        });
-    });
+    applyDB(pkgInfo,service, pkgInfo.name+':1');
+    applyEventservice(pkgInfo,service,config.datapath);
 })();
