250a18d35fae84c28c19c3ee133a8a9e035093af
LeeHanSeong <jkhk7788@gmail.com>
2021-08-01 01:41:22 +0900

    update

--- jsService/modules/eventModule.js
+++ jsService/modules/eventModule.js
@@ -9,13 +9,14 @@ function initEventModule(job_daystarter,path){
     eventList = readEventData();
     // eventList.forEach(i=>{
     //     addJobs(index);
-    // })
+    // });
 }
 
 class eventInfo{
-    constructor(device_id, action_id, contents, start, end){
+    constructor(device_id, action_id, title, contents, start, end){
         this.device_id = device_id;
         this.action_id = action_id;
+        this.title = title,
         this.contents = contents;
         this.start = start;
         this.end = end;
